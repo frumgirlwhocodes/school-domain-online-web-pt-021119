4ddeaba9b2639dfe0191dee001e73ddead87aa68
lieby weintraub <liebahinda@gmail.com>
2019-03-01 05:37:55 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -8,6 +8,7 @@ class School
   end 
   def add_student(student_name, student_grade)
     roster={}
+    if roster.lengh == 0 
     roster[student_grade]=[]
     roster[student_grade] << student_name
   end 
