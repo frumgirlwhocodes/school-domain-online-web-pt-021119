b6b086c1f50d48fc58ddb4fd474226d7611d1028
lieby weintraub <liebahinda@gmail.com>
2019-03-01 06:09:45 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -6,7 +6,7 @@ class School
     @roster={}
   end
   def add_student(name, grade)
-   if @roster.empty? == false
+   if @roster.has_key? == false
      @roster[grade]=[] 
      @roster[grade] << name
    else 
