f9469b151cef3036892f2f7f215f3576c871bc45
lieby weintraub <liebahinda@gmail.com>
2019-03-01 05:32:23 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -6,5 +6,8 @@ class School
   def roster
     roster={}
   end 
-  def add_student(student_name, student_age)
-    roster[student_name]
+  def add_student(student_name, student_grade)
+    roster[student_grade]=[]
+    roster << student_name
+  end 
+  end 
